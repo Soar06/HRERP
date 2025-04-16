edd1bb0bf476604357343c76e2025ecf4da45361
Vo Huu Luan <119402962+Soar06@users.noreply.github.com>
2025-04-16 18:31:55 +0700

    small adjust

--- HR.Infrastructure/obj/Debug/net9.0/HR.Infrastructure.AssemblyInfo.cs
+++ HR.Infrastructure/obj/Debug/net9.0/HR.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HR.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49872bba4f792ef8fdf3c53b5ecc88bdac262239")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2843c87beef0ae982891e0a1224b79d154a41f8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HR.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HR.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- HR.Tests/obj/Debug/net9.0/HR.Tests.AssemblyInfo.cs
+++ HR.Tests/obj/Debug/net9.0/HR.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HR.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b71d04f680689d00b5c0154560b9e12e1a12f6a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2843c87beef0ae982891e0a1224b79d154a41f8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HR.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HR.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
